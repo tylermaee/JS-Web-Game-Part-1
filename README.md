33e9a318d2e768be0a793a821ac74ae98221bf1c
Tyler <tylermaee0@gmail.com>
2023-11-29 15:10:48 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Game I
+
+This is an activity for SD Lesson 5.3.3- Function Practice
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ index.js
@@ -0,0 +1,29 @@
+function newImage(image, left, bottom) {
+    let object = document.createElement('img')
+    object.src = image
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object   
+}
+
+function newItem(image, left, bottom){
+    let object = newImage(image, left, bottom)
+    
+    object.addEventListener('dblclick', function() {
+        object.remove()
+    })
+}
+
+
+newImage('./assets/green-character.gif', 100, 100)
+newImage('./assets/pine-tree.png', 450, 200)
+newImage('./assets/tree.png', 200, 300)
+newImage('./assets/pillar.png', 350, 100)
+newImage('./assets/crate.png', 150, 200)
+newImage('./assets/well.png', 500, 425)
+
+newItem('./assets/sword.png',  500, 405)
+newItem('./assets/shield.png',  165, 185)
+newItem('./assets/staff.png',  600, 100)
